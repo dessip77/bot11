103be7bb33bafdb1d9c70aca92c1937682991f83
dessip77 <81189109+dessip77@users.noreply.github.com>
2021-03-27 18:13:09 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const request = require('request')
+const convertRupiah = require('rupiah-format')
+var moment = require('moment'); // require
+let url = 'https://vip.bitcoin.co.id/api/btc_idr/ticker'
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    qrcode.generate(qr,{small: true}, function (qrcode) {
+    console.log(qrcode);
+    });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    // Start Command
+    if (msg.body == '!cek') {
+        request({'url':url, 'json': true }, function (error, response, body) {
+            let date = moment.unix(body.ticker.server_time).format('dddd, MMMM Do, YYYY h:mm:ss A'); 
+            let high = convertRupiah.convert(body.ticker.high)
+            let low = convertRupiah.convert(body.ticker.low)
+            let vol_btc = convertRupiah.convert(body.ticker.vol_btc)
+            let vol_idr = convertRupiah.convert(body.ticker.vol_idr)
+            let last = convertRupiah.convert(body.ticker.last)
+            let buy = convertRupiah.convert(body.ticker.buy)
+            let sell = convertRupiah.convert(body.ticker.sell)
+          
+const txt = `*BTC Update Check*
+High: *${high}*
+low : *${low}* 
+Vol_BTC : *${body.ticker.vol_btc}* 
+Vol_IDR : *${vol_idr}* 
+Last : *${last}* 
+Buy : *${buy}* 
+Sell : *${sell}* 
+Date : *${date}* 
+`
+            msg.reply(txt)
+          });
+    }
+    // End Command
+});
+
+client.initialize();
